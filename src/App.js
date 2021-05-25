3944b89eb8993943326dbfb07c8e0be697c7f6b6
Edita Keškić <edita.keskic@gmail.com>
2021-05-25 21:31:53 +0200

    Deleting const

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 
 function App() {
-  const name = 'Edita'
-  const x = false
-
   return (
     <div className='container'>
       <h1>Hello From React</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+      <h2>Hello</h2>
     </div>
   );
 }
